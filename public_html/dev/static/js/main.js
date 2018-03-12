77e5766c426a340d3c5753e65d3a1866a25130ae
Elena Chaya <elenachaya@012smile.net>
2018-03-12 22:25:50 +0200

    add portfolio section

--- public_html/dev/static/js/main.js
+++ public_html/dev/static/js/main.js
@@ -1,2 +1,56 @@
 ;
 
+$(document).ready(function () {
+    
+    $('.js-portfolio-video-slider').slick({
+        dots: true,
+        arrows: false
+    });
+    
+    $('.js-portfolio-photo-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        prevArrow: '.portfolio-photo__prev',
+        nextArrow: '.portfolio-photo__next',   
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }]
+    });
+    
+    $('.js-portfolio-photo-slider').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        image: {
+            verticalFit: true
+        },
+        gallery: {
+            enabled: true
+            
+        }
+        
+        
+        
+    });
+    
+});
+
