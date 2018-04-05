f1446299abef567b2453db925add8e5fd22921d4
Elena Chaya <elenachaya@012smile.net>
2018-04-05 14:08:18 +0300

    correct slider

--- public_html/dev/static/js/main.js
+++ public_html/dev/static/js/main.js
@@ -18,14 +18,67 @@ $(document).ready(function () {
         }
     });
     
-    $('.js-video-slider').slick({
-        dots: true,
-        arrows: false
-    });
+//    $('.js-video-slider').slick({
+//        dots: true,
+//        arrows: false
+//    });
+//
+//    $('.js-portfolio-video-slider').slick({
+//        dots: true,
+//        prevArrow: '.portfolio-video__prev',
+//        nextArrow: '.portfolio-video__next',
+//        responsive: [{
+//                breakpoint: 700,
+//                settings: {
+//                    dots:false
+//                }
+//        }]
+//    });
+    function sliderVideo(name) {
+        $('.js-'+name).slick({
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           arrows:true,
+           prevArrow: '.'+name+'__prev',
+           nextArrow: '.'+name+'__next', 
+           dots: true,
+           responsive: [{
+               breakpoint: 700,
+               settings: {
+                   arrows:true,
+                   prevArrow: '.mob-'+name+'__prev',
+                   nextArrow: '.mob-'+name+'__next', 
+                   dots: false
+               }
+           }]
+        });
+        
+        
+        $('.' + name).on("click touchend", ".js-"+name+" .slick-dots, .slider__controlls, .mobile__controlls", function () {
+            var iframe_id = [];
+            $('.js-' + name+' .video__item iframe').each(function (key, value) {
+                if (value.id.length > 0) {
+                    iframe_id.push(value.id);
+                }
+            });
+            $.each(iframe_id, function (key, value) {
+                $('.slick-track')
+                        .find('div:not(.slick-cloned)')
+                        .find('#' + iframe_id[key])[0]
+                        .contentWindow
+                        .postMessage('{"event":"command", "func":"' + 'pauseVideo' + '","args":""}', '*');
+            });
+        });
+    
+    }
+    
+    sliderVideo('portfolio-video-slider');
+    sliderVideo('reviews-video-slider');
     
     $('.js-portfolio-photo-slider').slick({
         slidesToShow: 5,
         slidesToScroll: 1,
+        arrows:true,
         prevArrow: '.portfolio-photo__prev',
         nextArrow: '.portfolio-photo__next',  
         responsive: [{
@@ -34,9 +87,18 @@ $(document).ready(function () {
                 slidesToShow: 3,
                 slidesToScroll: 3,
                 infinite: true,
-                dots: true
+                dots: false
             }
-        }, {
+        },{
+            breakpoint: 700,
+            settings: {
+                arrows:true,
+                prevArrow: '.mob-portfolio-photo__prev',
+                nextArrow: '.mob-portfolio-photo__next',
+                slidesToShow: 3,
+                slidesToScroll: 3
+            }
+        },{
             breakpoint: 600,
             settings: {
                 slidesToShow: 2,
@@ -86,22 +148,16 @@ $(document).ready(function () {
         }
     });
     
-    var iframe_id = [];
-    $('.video__item iframe').each(function(key, value) {
-        if(value.id.length > 0) {
-            iframe_id.push(value.id);
-        }
-    });
+//    var iframe_id = [];
+//    $('.video__item iframe').each(function(key, value) {
+//        if(value.id.length > 0) {
+//            iframe_id.push(value.id);
+//        }
+//    });
+//    console.log(iframe_id);
+
+  
     
-    $('.video-slider').on("click", ".js-video-slider .slick-dots", function() {
-        $.each(iframe_id, function(key, value) {
-            $('.slick-track')
-                    .find('div:not(.slick-cloned)')
-                    .find('#' + iframe_id[key])[0]
-                    .contentWindow
-                    .postMessage('{"event":"command", "func":"' + 'pauseVideo' + '","args":""}', '*');
-        });
-    });
     
     function questions() {
         var question = $('.question-title'),
