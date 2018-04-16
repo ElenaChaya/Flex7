7742fee1b803f1b4276838fc8dd8c3439a441f27
Elena Chaya <elenachaya@012smile.net>
2018-04-16 22:54:40 +0300

    mobile version and fixes

--- public_html/dev/static/js/main.js
+++ public_html/dev/static/js/main.js
@@ -45,10 +45,10 @@ $(document).ready(function () {
            responsive: [{
                breakpoint: 700,
                settings: {
+                   dots: false,
                    arrows:true,
                    prevArrow: '.mob-'+name+'__prev',
                    nextArrow: '.mob-'+name+'__next', 
-                   dots: false
                }
            }]
         });
@@ -120,12 +120,142 @@ $(document).ready(function () {
     });
     
     
-    $('.js-reviews-slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: '.slider__prev',
-        nextArrow: '.slider__next'
-    });
+    
+    function sliderReviews() {
+        $('.js-reviews-slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            prevArrow: '.reviews-slider__prev',
+            nextArrow: '.reviews-slider__next',
+            useTransform: false,
+            infinite: false,
+            responsive: [{
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: false,
+                dots: false,
+                prevArrow: '.reviews-slider-mobile__prev',
+                nextArrow: '.reviews-slider-mobile__next'
+                
+                
+            }
+        }]
+//            customPaging: function (slider, i) {
+//            //FYI just have a look at the object to find available information
+//            //press f12 to access the console in most browsers
+//            //you could also debug or look in the source
+//            console.log('slider');
+//            console.log(slider);
+//            console.log(slider.slideCount);
+//        }
+
+
+        });
+        
+    }
+  //  $('.add-remove').slick('slickAdd','<div><h3>' + slideIndex + '</h3></div>');
+   
+    function review(resize) {
+        var elems = $(".review-wrap");
+        var wrapper = $('<div class="review-slider__item" />');
+        var pArrLen = elems.length;
+        var size = $(window).width();
+        
+        if ((size <700) && (resize)) {
+            if($('.js-reviews-slider').hasClass("big")) {
+                $('.js-reviews-slider').slick('unslick');
+                $('.review-wrap').unwrap();
+                console.log('kkkjjkj');
+                
+//                for (var j = 0; j < pArrLen; j ++) {
+//                    elems.filter(':eq(' + j + ')').wrapAll(wrapper);
+//                }
+                sliderReviews();
+                $('.js-reviews-slider').removeClass( "big" ).addClass("small");
+//                if($('.js-reviews-slider').hasClass("first")) {
+//                    $('.js-reviews-slider').removeClass( "first" );
+//                }
+                console.log("<700 resize big");
+            } else {
+                console.log("<700 resize !big");
+            }
+
+        }else if ((size >= 700) && (resize)) {
+
+            if ($('.js-reviews-slider').hasClass("small")) {
+                $('.js-reviews-slider').slick('unslick');
+//                if (!$('.js-reviews-slider').hasClass("first")) {
+//                    $('.review-wrap').unwrap();
+//                   // $('.js-reviews-slider').removeClass( "first" );
+//                }
+                for (var i = 0; i < pArrLen; i += 2) {
+                    elems.filter(':eq(' + i + '),:eq(' + (i + 1) + ')').wrapAll(wrapper);
+                }
+                sliderReviews();
+                $('.js-reviews-slider').removeClass("small").addClass("big");
+                console.log(">=700 resize small");
+            } else {
+                console.log(">=700 resize !small");
+            }
+            
+        } else if ((size < 700) && !resize){
+            
+            //$('.js-reviews-slider').slick('unslick');
+            
+            $('.review-wrap').unwrap();
+            sliderReviews();
+            $('.js-reviews-slider').addClass("small");
+            console.log("<700 !resize");
+            
+        } else if ((size >= 700) && !resize){
+            sliderReviews();
+            $('.js-reviews-slider').addClass("big");
+            console.log(">=700 !resize");
+        }
+    }
+    
+    stagesSlider();
+
+    review(0);
+
+    $(window).resize(function() {
+        review(1);
+        stagesSlider();
+     });
+        
+    function stagesSlider() {
+        if (($(window).width() < 700) && !($('.js-stages-item__wrap').hasClass('slick-initialized'))) {
+            $('.js-stages-item__wrap').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                prevArrow: '.mob-stages__prev',
+                nextArrow: '.mob-stages__next'
+            });
+        } else if(($(window).width() > 700) && ($('.js-stages-item__wrap').hasClass('slick-initialized'))) {
+           $('.js-stages-item__wrap').slick('unslick'); 
+        }
+
+
+
+        
+        
+       
+        
+//        } else {
+//            console.log($('.js-stages-item__wrap').slick);
+//            $('.js-stages-item__wrap').slick('unslick');
+//        }   
+    }
+        
+        
+    
+ 
+//    $(window).on('resize', review);
+    
+    
+    
 //    $('.js-portfolio-photo-slider').slick({
 //        slidesToShow: 3,
 //        slidesToScroll: 1,
@@ -166,33 +296,35 @@ $(document).ready(function () {
             var curAnswer = $(this).siblings(answer);
             if(curAnswer.is(':visible')) {
                 curAnswer.slideUp();
+                $(this).removeClass('active');
             } else {
                 curAnswer.slideDown();
+                $(this).addClass('active');
             }
         });
     }
     
     questions();
     
-    $("form").on("submit", function() {
-        var formID = '#' + $(this).attr("id");
-        $(formID).validate({
-            rules: {
-                name: 'required',
-                phone: 'required'
-            },
-            messages: {
-                name: '',
-                phone: ''
-            }
-        });
-        if($(formID).valid()) {
-       
-           $(formID).parent().hide();
-           $(formID).parent().parent().find('.popup-success').addClass('success-show');
-        }
-        return false;
-    });
+//    $("form").on("submit", function() {
+//        var formID = '#' + $(this).attr("id");
+//        $(formID).validate({
+//            rules: {
+//                name: 'required',
+//                phone: 'required'
+//            },
+//            messages: {
+//                name: '',
+//                phone: ''
+//            }
+//        });
+//        if($(formID).valid()) {
+//       
+//           $(formID).parent().hide();
+//           $(formID).parent().parent().find('.popup-success').addClass('success-show');
+//        }
+//        return false;
+//    });
     
 });
 
